708fa1201cda47ad6766184399cc61b28c786357
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-01-31 14:17:23 +0800

    *: update pom *: update test main

--- source/test/cpp/test_main.cpp
+++ source/test/cpp/test_main.cpp
@@ -8,10 +8,67 @@ using namespace xcore;
 UNITTEST_SUITE_LIST(xRandomUnitTest);
 UNITTEST_SUITE_DECLARE(xRandomUnitTest, xrandom);
 
-s32 main(s32 argc, char** argv)
+class UnitTestAllocator : public UnitTest::Allocator
 {
-	UnitTest::TestReporterStdout reporter;
-	s32 r = UNITTEST_SUITE_RUN(reporter, xRandomUnitTest);
+public:
+	xcore::x_iallocator*	mAllocator;
+	int						mNumAllocations;
 
+	UnitTestAllocator(xcore::x_iallocator* allocator)
+		: mNumAllocations(0)
+	{
+		mAllocator = allocator;
+	}
+
+	void	Release()
+	{
+		mAllocator->release();
+	}
+
+	void*	Allocate(int size)
+	{
+		++mNumAllocations;
+		return mAllocator->allocate(size, 4);
+	}
+	void	Deallocate(void* ptr)
+	{
+		--mNumAllocations;
+		mAllocator->deallocate(ptr);
+	}
+};
+
+
+class UnitTestObserver : public UnitTest::Observer
+{
+public:
+	void	BeginFixture(const char* filename, const char* suite_name, const char* fixture_name)
+	{
+	}
+	void	EndFixture()
+	{
+	}
+};
+
+
+int main(int argc, char** argv)
+{
+	UnitTestAllocator unittestAllocator( xcore::gCreateSystemAllocator() );
+	UnitTestObserver unittestObserver;
+	UnitTest::SetAllocator(&unittestAllocator);
+	UnitTest::SetObserver(&unittestObserver);
+
+	UnitTest::TestReporterStdout stdout_reporter;
+	UnitTest::TestReporter& reporter = stdout_reporter;
+
+	int r = UNITTEST_SUITE_RUN(reporter, xRandomUnitTest);
+
+	if (unittestAllocator.mNumAllocations!=0)
+	{
+		reporter.reportFailure(__FILE__, __LINE__, __FUNCTION__, "memory leaks detected!");
+		r = -1;
+	}
+
+	unittestAllocator.Release();
 	return r;
+
 }
