8775a7c694ae0b65443b79b3a60020b206a006d4
jurgen.kluft <jurgen.kluft@gmail.com>
2023-05-18 20:15:24 +0800

    * crandom; fixed

--- package/package.go
+++ package/package.go
@@ -2,7 +2,8 @@ package crandom
 
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
-	"github.com/jurgen-kluft/ccode/denv"
+	denv "github.com/jurgen-kluft/ccode/denv"
+	ccore "github.com/jurgen-kluft/ccore/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
@@ -11,20 +12,24 @@ func GetPackage() *denv.Package {
 	// Dependencies
 	cunittestpkg := cunittest.GetPackage()
 	cbasepkg := cbase.GetPackage()
+	ccorepkg := ccore.GetPackage()
 
 	// The main (crandom) package
 	mainpkg := denv.NewPackage("crandom")
 	mainpkg.AddPackage(cunittestpkg)
 	mainpkg.AddPackage(cbasepkg)
+	mainpkg.AddPackage(ccorepkg)
 
 	// 'crandom' library
 	mainlib := denv.SetupDefaultCppLibProject("crandom", "github.com\\jurgen-kluft\\crandom")
 	mainlib.Dependencies = append(mainlib.Dependencies, cbasepkg.GetMainLib())
+	mainlib.Dependencies = append(mainlib.Dependencies, ccorepkg.GetMainLib())
 
 	// 'crandom' unittest project
 	maintest := denv.SetupDefaultCppTestProject("crandom_test", "github.com\\jurgen-kluft\\crandom")
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, ccorepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
